ceba739f4153179e3fc79bf487ef99775f913cfe
0xKubitus <n.cubilier@gmail.com>
2022-09-22 15:16:44 +0200

    fix searchfooddata

--- src/components/SearchFoodData.jsx
+++ src/components/SearchFoodData.jsx
@@ -26,6 +26,7 @@ const SearchFoodData = () => {
                 // console.log('data type =', typeof data); // SHOULD BE A COMMENT
                 console.log('data = ', data); // SHOULD BE A COMMENT
                 console.log('nutriments du 1er resultat de la recherche = ', data.hints[0].food.nutrients); // SHOULD BE A COMMENT
+
                 setSearchResult(data.hints[0]);
             } else if (response.status === 404) {
                 console.log('Error 404 - Not Found');
@@ -61,36 +62,34 @@ const SearchFoodData = () => {
                     </form>
                 </section>
             ) : (
-                // <>
-                //     <section>
-                //         <h1>{searchResult.food.label} - Détails</h1>
-                //         <img
-                //             src={searchResult.food.image}
-                //             alt="search-result"
-                //         />
-                //         <p>Pour une unité (ou une portion) :</p>
-                //         <p>Calories = {searchResult.food.nutrients.ENERC_KCAL}kcal</p>
-                //         <p>Proteines = {searchResult.food.nutrients.PROCNT}g</p>
-                //         <p>Lipides = {searchResult.food.nutrients.FAT}g</p>
-                //         <p>Glucides = {searchResult.food.nutrients.CHOCDF}g</p>
-                //         <p>Fibres = {searchResult.food.nutrients.FIBTG}g</p>
-                //     </section>
+                <>
+                    <section>
+                        <h1>{searchResult.food.label} - Détails</h1>
+                        <img
+                            src={searchResult.food.image}
+                            alt="search-result"
+                        />
+                        <p>Pour une unité (ou une portion) :</p>
+                        <p>Calories = {searchResult.food.nutrients.ENERC_KCAL}kcal</p>
+                        <p>Proteines = {searchResult.food.nutrients.PROCNT}g</p>
+                        <p>Lipides = {searchResult.food.nutrients.FAT}g</p>
+                        <p>Glucides = {searchResult.food.nutrients.CHOCDF}g</p>
+                        <p>Fibres = {searchResult.food.nutrients.FIBTG}g</p>
+                    </section>
 
-                //     <section className="search-food-data">
-                //         <form onSubmit={handleFoodSearchFormSubmit}>
-                //             <label>Rechercher les apports d'un aliment :</label>
-                //             <input
-                //                 type="text"
-                //                 value={searchInput}
-                //                 name="search-food-data-input"
-                //                 onChange={(e) => setSearchInput(e.target.value)}
-                //             />
-                //             <button>Rechercher</button>
-                //         </form>
-                //     </section>
-                // </>
-
-                <div>{/* à refaire avec .map sur chaque pour chaque hint -> plusieurs foods et measurements + utiliser la 2eme requete en POST */}</div>
+                    <section className="search-food-data">
+                        <form onSubmit={handleFoodSearchFormSubmit}>
+                            <label>Rechercher les apports d'un aliment :</label>
+                            <input
+                                type="text"
+                                value={searchInput}
+                                name="search-food-data-input"
+                                onChange={(e) => setSearchInput(e.target.value)}
+                            />
+                            <button>Rechercher</button>
+                        </form>
+                    </section>
+                </>
             )}
         </>
     );
