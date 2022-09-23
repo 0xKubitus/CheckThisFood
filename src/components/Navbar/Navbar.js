74ce710dfdcc073dcec3ce011e163342b34f5696
Eva <eva.laurent.78@gmail.com>
2022-09-23 12:52:48 +0200

    button navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,114 @@
 import { Link } from 'react-router-dom';
+import * as React from 'react';
 import { useAtomValue } from 'jotai';
 import { isUserLogged } from 'atoms/userStatus';
 import LogoutBtn from './LogoutBtn';
-import "./Navbar.css";
+import './Navbar.css';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import HowToRegIcon from '@mui/icons-material/HowToReg';
+import Divider from '@mui/material/Divider';
+import VpnKeyIcon from '@mui/icons-material/VpnKey';
+import LogoutIcon from '@mui/icons-material/Logout';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { styled, alpha } from '@mui/material/styles';
+
+const StyledMenu = styled((props) => (
+    <Menu
+        elevation={0}
+        anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+        }}
+        transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+        }}
+        {...props}
+    />
+))(({ theme }) => ({
+    '& .MuiPaper-root': {
+        borderRadius: 6,
+        marginTop: theme.spacing(1),
+        minWidth: 200,
+        color: theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+        boxShadow: 'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+        '& .MuiMenu-list': {
+            padding: '4px 0',
+        },
+        '& .MuiMenuItem-root': {
+            '& .MuiSvgIcon-root': {
+                fontSize: 18,
+                color: theme.palette.text.secondary,
+                marginRight: theme.spacing(1.5),
+            },
+            '&:active': {
+                backgroundColor: alpha(theme.palette.primary.main, theme.palette.action.selectedOpacity),
+            },
+        },
+    },
+}));
 
 const Navbar = () => {
     const userStatus = useAtomValue(isUserLogged);
 
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
-        <nav className='navbar-main'>
-            {!userStatus ? (
+        <nav className="navbar-main">
+            <Button style = {{backgroundColor:'#2f4050'}}
+                id="demo-customized-button"
+                aria-controls={open ? 'demo-customized-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                variant="contained"
+                disableElevation
+                onClick={handleClick}
+                endIcon={<KeyboardArrowDownIcon />}>
+                S'inscrire / Se connecter
+            </Button>
+            <StyledMenu
+                id="demo-customized-menu"
+                MenuListProps={{
+                    'aria-labelledby': 'demo-customized-button',
+                }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}>
+                <MenuItem onClick={handleClose} disableRipple>
+                    <HowToRegIcon />
+
+                    <Link to="/signup" className="navbar-first-link" style={{color: '#2f4050'}}>
+                        S'inscrire
+                    </Link>
+                </MenuItem>
+                <MenuItem onClick={handleClose} disableRipple>
+                    <VpnKeyIcon />
+                    <Link to="/login" className="navbar-second-link" style={{color: '#2f4050'}}>
+                        Se connecter
+                    </Link>
+                </MenuItem>
+
+                <Divider sx={{ my: 0.5 }} />
+
+                <MenuItem onClick={handleClose} disableRipple>
+                    <LogoutIcon />
+                    {/* <LogoutBtn /> */}
+                    Se déconnecter
+                </MenuItem>
+            </StyledMenu>
+
+            {/* {!userStatus ? (
                 <span>
-                    <Link to ="/signup" className='navbar-first-link'>
+                    <Link to ="/signup" style={{ textDecoration: 'none' }}className='navbar-first-link'>
 
                     S'inscrire
                     </Link>
@@ -23,11 +120,9 @@ const Navbar = () => {
                 <span>
                     <LogoutBtn />
                 </span>
-            )}
+            )} */}
         </nav>
     );
-
 };
-
 
 export default Navbar;
