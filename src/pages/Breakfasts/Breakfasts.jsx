4a1e1722c2b65dddc0b48e014b970eacab531648
Alexis-Fourthies <alexisfourthies@gmail.com>
2022-09-24 20:02:07 +0200

    improve sidebar, add grid to other recipes

--- src/pages/Breakfasts/Breakfasts.jsx
+++ src/pages/Breakfasts/Breakfasts.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'api/axios';
 import Recipes from 'components/Recipes';
+import { Grid } from '@mui/material';
 
 const dev_backend_url = 'http://localhost:3001';
 const production_backend_url = 'https://repoback.herokuapp.com';
@@ -17,9 +18,11 @@ const Breakfast = () => {
     console.log(breakfastRecipes); // SHOULD BE A COMMENT
     return (
         <div>
+            <Grid container sx={{ justifyContent: 'center', padding: '5px' }}>
             {breakfastRecipes.map((item) => (
                 <Recipes title={item.attributes.title} description={item.attributes.description} carbohydrates={item.attributes.carbohydrates} calories={item.attributes.calories} image={item.attributes.image_url} id={item.id} />
             ))}
+            </Grid>
         </div>
     );
 };
