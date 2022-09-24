f368a060e0f0d9a4c662c327ef4d8fefc78f7117
Alexis-Fourthies <alexisfourthies@gmail.com>
2022-09-24 16:30:35 +0200

    temporary fix + redesigned sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,16 +5,14 @@ import logo from 'assets/images/Logoctf.png'
 import MenuIcon from '@mui/icons-material/Menu';
 
 function Sidebar() {
-  const[isOpen, setIsOpen] = useState(false);
-  const toggle = () => setIsOpen (!isOpen);
+  
+  
   return (
-    <div style={{width: isOpen ? "250px" : "50px"}} className="Sidebar">
-      <div style={{marginLeft: isOpen ? "0px" : "0px" }} className="bars">
 
-        <MenuIcon onClick={toggle}/>
-      </div>
-
-      <img style={{display: isOpen ? "block" : "none"}} src={logo} width={250} height={250} alt="Logo"/>
+    <div className="Sidebar">
+  
+    <div className="Logohidden">
+      <img src={logo} width={250} height={250} alt="Logo"/></div>
       <ul className="SidebarList">
       {SidebarData.map((val, key) => {
       return (
@@ -27,7 +25,7 @@ function Sidebar() {
           }}
           >
           <div id="icon">{val.icon}</div>{""}
-          <div style={{display: isOpen ? "block" : "none"}} id="title">
+          <div id="title">
             {val.title}
           </div>
 
